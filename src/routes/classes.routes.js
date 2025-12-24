51594561f0fa6855b3d82b638bc58a47a08549a5
Arthana12061999 <arthanarieswara_predart@outlook.com>
2025-12-24 12:16:45 +0530

    added Class Adviser block

--- src/routes/classes.routes.js
+++ src/routes/classes.routes.js
@@ -2,11 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { verifyToken, isAdmin } = require('../middleware/auth.middleware');
-const { getClasses, createClass } = require('../controllers/classes.controller');
+
+const {
+  getClasses,
+  createClass,
+  getClassesByYear,
+} = require('../controllers/classes.controller');
 
 router.get('/classes', verifyToken, isAdmin, getClasses);
 router.post('/classes', verifyToken, isAdmin, createClass);
 router.get('/classes/by-year', verifyToken, isAdmin, getClassesByYear);
-
 
 module.exports = router;
