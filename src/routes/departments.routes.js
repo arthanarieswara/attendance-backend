514efd259be410f8b945a02178e496989db74851
Arthana12061999 <arthanarieswara_predart@outlook.com>
2025-12-24 11:16:37 +0530

    added Classes

--- src/routes/departments.routes.js
+++ src/routes/departments.routes.js
@@ -2,14 +2,15 @@ const express = require('express');
 const router = express.Router();
 
 const { verifyToken, isAdmin } = require('../middleware/auth.middleware');
+
 const {
   getDepartments,
   createDepartment,
+  updateDepartment,
 } = require('../controllers/departments.controller');
 
 router.get('/departments', verifyToken, isAdmin, getDepartments);
 router.post('/departments', verifyToken, isAdmin, createDepartment);
 router.put('/departments/:id', verifyToken, isAdmin, updateDepartment);
-
 
 module.exports = router;
